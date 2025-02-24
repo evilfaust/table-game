0447c4df50b020f118354f26518af718e0d9474a
Oleg Pavlyuchenko <oleg.faust@gmail.com>
2025-02-24 23:47:33 +1100

    fix

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Row, Col, Button, Typography, Card } from "antd";
+import {Row, Col, Button, Typography, Card, Avatar} from "antd";
 import { RocketOutlined } from "@ant-design/icons";
 import "antd/dist/reset.css";
+import {Link} from "react-router-dom";
 
 const { Title, Paragraph } = Typography;
 
@@ -15,11 +16,17 @@ const HeroSection = () => {
         <Col xs={24} lg={12}>
           <Card className="hero-card text-left" bordered={false}>
             <div className="badge text-white">
-              <RocketOutlined className="icon" /> Киберспорт это спорт
+              <RocketOutlined className="icon"/> Киберспорт это спорт
             </div>
             <Title level={3} className="text-white">
               КИБЕР-СПОРТИВНАЯ <span className="highlight">ЛИГА</span>
             </Title>
+            <div style={{ justifyContent: "center" }}>
+              <Avatar shape="square" size={64} src="/images/icon001.png" style={{ marginRight: "5px" }}/>
+              <Avatar shape="square" size={64} src="/images/icon002.png" style={{ marginRight: "5px" }}/>
+              <Avatar shape="square" size={64} src="/images/icon003.png" style={{ marginRight: "5px" }}/>
+              <Avatar shape="square" size={64} src="/images/icon004.png" style={{ marginRight: "5px" }}/>
+            </div>
             <Title level={2} className="hero-title-game text-white">
               EMCO.TECH<span className="highlight">.SPORTS</span>
             </Title>
@@ -32,22 +39,22 @@ const HeroSection = () => {
             <Row gutter={[16, 16]} justify="center">
               <Col xs={24} sm={12}>
                 <Button
-                  block 
-                  variant="solid"
-                  href="https://forms.yandex.ru/cloud/67b02371f47e73d61ea520c8/"
-                  style={{backgroundColor: "#FEA202"}}
-                  color="#FEA202"
+                    block
+                    variant="solid"
+                    href="https://forms.yandex.ru/cloud/67b02371f47e73d61ea520c8/"
+                    style={{backgroundColor: "#FEA202"}}
+                    color="#FEA202"
                 >
                   DOTA 2
                 </Button>
               </Col>
               <Col xs={24} sm={12}>
                 <Button
-                  block 
-                  variant="solid"
-                  href="https://forms.yandex.ru/cloud/67b01cfb02848fd446b03ba8/"
-                  color="#FEA202"
-                  style={{backgroundColor: "#FEA202"}}
+                    block
+                    variant="solid"
+                    href="https://forms.yandex.ru/cloud/67b01cfb02848fd446b03ba8/"
+                    color="#FEA202"
+                    style={{backgroundColor: "#FEA202"}}
                 >
                   CS 2
                 </Button>
