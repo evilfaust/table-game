a6fb384b2ad1d3913b094701318575e838654e07
evilfaust <oleg.faust@gmail.com>
2025-02-25 19:42:02 +1100

    footer

--- src/components/EsportsFooter.js
+++ src/components/EsportsFooter.js
@@ -1,86 +1,115 @@
-// import "./esportsfooter.css";
 import React from "react";
 import { Layout } from "antd";
-import {
-  YoutubeOutlined,
-  InstagramOutlined,
-  DribbbleOutlined,
-} from "@ant-design/icons";
 import "./esportsfooter.css";
-// import "./Footer.css";
 
 const { Footer } = Layout;
 
 const EsportsFooter = () => {
   return (
-    <Footer className="spectacledcoder-footer">
-      <div className="waves">
-        <div className="wave" id="wave1"></div>
-        <div className="wave" id="wave2"></div>
-        <div className="wave" id="wave3"></div>
-        <div className="wave" id="wave4"></div>
+    <footer>
+      {/* Background SVG */}
+      <div className="background">
+        <svg
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink" // Используем camelCase для xmlns:xlink
+          x="0px"
+          y="0px"
+          width="100%"
+          height="100%"
+          viewBox="0 0 3200 900" // Увеличиваем viewBox по ширине
+        >
+          <defs>
+            <path
+              id="wave"
+              fill="rgba(105, 27, 252, 0.6)"
+              d="M-727.704,502.589c0,0,473.976-41.997,1010.95,0
+                s743.962,38.998,1151.942,0s587.97-39.278,1010.948,5.859s986.95,48.368,1433.926-4.995v560.106H-727.704V502.589z"
+            />
+          </defs>
+          <g>
+            <use href="#wave" opacity=".4">
+              <animateTransform
+                attributeName="transform"
+                attributeType="XML"
+                type="translate"
+                dur="12s" // Увеличиваем длительность анимации
+                calcMode="spline"
+                values="0 230; -668 180; 0 230" // Увеличиваем значения перемещения
+                keyTimes="0; .5; 1"
+                keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                repeatCount="indefinite"
+              />
+            </use>
+            <use href="#wave" opacity=".6">
+              <animateTransform
+                attributeName="transform"
+                attributeType="XML"
+                type="translate"
+                dur="10s" // Увеличиваем длительность анимации
+                calcMode="spline"
+                values="0 230; 486 220; 0 230" // Увеличиваем значения перемещения
+                keyTimes="0; .6; 1"
+                keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                repeatCount="indefinite"
+              />
+            </use>
+            <use href="#wave" opacity=".9">
+              <animateTransform
+                attributeName="transform"
+                attributeType="XML"
+                type="translate"
+                dur="8s" // Увеличиваем длительность анимации
+                calcMode="spline"
+                values="0 230; -280 200; 0 230" // Увеличиваем значения перемещения
+                keyTimes="0; .4; 1"
+                keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                repeatCount="indefinite"
+              />
+            </use>
+          </g>
+        </svg>
       </div>
 
-      <div className="spectacledcoder-footer-content">
-        <div className="spectacledcoder-footer-section">
-          <h1 className="spectacledcoder-footer-heading">EMCO.TECH.SPORTS</h1>
-        </div>
-        <div className="spectacledcoder-footer-section">
-          <ul className="d-footer-ul">
-            <li className="d-footer-li-h"><b>Links</b></li>
-            <li className="d-footer-li">Home</li>
-            <li className="d-footer-li">About</li>
-            <li className="d-footer-li">Blog</li>
-            <li className="d-footer-li">Design</li>
-            <li className="d-footer-li">Documentation</li>
-          </ul>
-        </div>
-        <div className="spectacledcoder-footer-section">
-          <ul className="d-footer-ul">
-            <li className="d-footer-li-h"><b>Blog</b></li>
-            <li className="d-footer-li">UI / UX</li>
-            <li className="d-footer-li">CodePens</li>
-            <li className="d-footer-li">Codedamn</li>
-            <li className="d-footer-li">Figma</li>
-            <li className="d-footer-li">Oracle EBS</li>
-          </ul>
-        </div>
-        <div className="spectacledcoder-footer-section">
-          <ul className="d-footer-ul">
-            <li className="d-footer-li-h"><b>Privacy policy</b></li>
-            <li className="d-footer-li-h"><b>Contact Us</b></li>
-          </ul>
-        </div>
-        <div className="spectacledcoder-footer-section">
-          <div className="logo-head">
-            <b>Follow us on</b>
-          </div>
-          <div className="logo">
-            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-              <InstagramOutlined className="social-icon" />
+      {/* Footer Content */}
+      <section>
+        <ul className="socials">
+          <li>
+            <a className="fa-brands fa-x-twitter" href="#twitter">
+              Twitter
             </a>
-            <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
-              <YoutubeOutlined className="social-icon" />
+          </li>
+          <li>
+            <a className="fa-brands fa-facebook" href="#facebook">
+              Facebook
             </a>
-            <a href="https://dribbble.com/" target="_blank" rel="noopener noreferrer">
-              <DribbbleOutlined className="social-icon" />
+          </li>
+          <li>
+            <a className="fa-brands fa-linkedin" href="#linkedin">
+              LinkedIn
             </a>
-          </div>
-        </div>
-      </div>
-
-      <div className="spectacledcoder-footer-bottom">
-        <div className="spectacledcoder-footer-bottom-sec">
-          <p>Icons by <a href="https://icons8.com/" target="_blank">Icons8</a></p>
-        </div>
-        <div className="spectacledcoder-footer-bottom-sec">
-          <p>Designed & Created by EMCO.TECH.SPORTS</p>
-        </div>
-        <div className="spectacledcoder-footer-bottom-sec">
-          <p>&copy; 2024 EMCO.TECH.SPORTS</p>
-        </div>
-      </div>
-    </Footer>
+          </li>
+        </ul>
+        <ul className="links">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="#skillset">Skillset</a>
+          </li>
+          <li>
+            <a href="#hire">Hire</a>
+          </li>
+        </ul>
+        <p className="legal">© 2024 All rights reserved</p>
+      </section>
+    </footer>
   );
 };
 
